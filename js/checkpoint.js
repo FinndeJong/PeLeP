ef82fe2090f3e302280639e0348ee42dd4f023d8
LAPTOP-AR13DN8H\LIEKE <lieke.crum@gmail.com>
2021-03-18 16:21:03 +0100

    dingen

--- js/checkpoint.js
+++ js/checkpoint.js
@@ -124,12 +124,16 @@ let emoji_boven = document.getElementById("boven");
 emoji_onder.addEventListener("click", function(){
     if(emoji != "onder_niveau"){
         emoji = "onder_niveau";
-        document.getElementById("onder").style.backgroundColor = "#cfdafc"
-        document.getElementById("onder").style.border = "thin black solid"
+        emoji_onder.style.backgroundColor = "#cfdafc"
+        emoji_onder.style.border = "thin black solid"
+        emoji_op.style.backgroundColor = "white"
+        emoji_boven.style.backgroundColor = "white"
         emoji_op.style.border = "none"
         emoji_boven.style.border = "none"
+        document.getElementById("foutmelding-emoji").innerText = ""
     } else{
         emoji_onder.style.border = "none"
+        emoji_onder.style.backgroundColor = "white"
         emoji = ""
     };
     console.log(emoji)
@@ -137,11 +141,16 @@ emoji_onder.addEventListener("click", function(){
 emoji_op.addEventListener("click", function(){
     if(emoji != "op_niveau"){
         emoji = "op_niveau";
-        emoji_op.style.border = "thin blue solid"
+        emoji_op.style.backgroundColor = "#cfdafc"
+        emoji_op.style.border = "thin black solid"
+        emoji_onder.style.backgroundColor = "white"
+        emoji_boven.style.backgroundColor = "white"
         emoji_onder.style.border = "none"
         emoji_boven.style.border = "none"
+        document.getElementById("foutmelding-emoji").innerText = ""
     } else{
         emoji_op.style.border = "none"
+        emoji_op.style.backgroundColor = "white"
         emoji = ""
     };
     console.log(emoji)
@@ -149,149 +158,164 @@ emoji_op.addEventListener("click", function(){
 emoji_boven.addEventListener("click", function(){
     if(emoji != "boven_niveau"){
         emoji = "boven_niveau";
-        emoji_boven.style.border = "thin blue solid"
+        emoji_boven.style.border = "thin black solid"
+        emoji_boven.style.backgroundColor = "#cfdafc"
         emoji_onder.style.border = "none"
         emoji_op.style.border = "none"
+        emoji_op.style.backgroundColor = "white"
+        emoji_onder.style.backgroundColor = "white"
+        document.getElementById("foutmelding-emoji").innerText = ""
     } else{
         emoji_boven.style.border = "none"
+        emoji_boven.style.backgroundColor = "white"
         emoji = ""
     };
     console.log(emoji)
 });
 
 function nieuw(){
-    if (emoji == ""){
-
-    }
-    var comp1 = "";
-    var comp2 = "";
-    var comp3 = "";
-    var comp4 = "";
-    var comp5 = "";
-    var comp6 = "";
-    console.log(comp1)
-    if (document.getElementById("Samenwerken-kies").style.display == 'inline-block'){
-        comp1 = "samenwerken"
-        console.log(1)
-        console.log(comp1)
-    } 
-    if (document.getElementById("Reflecteren-kies").style.display == 'inline-block'){
-        if (comp1 == ""){
-            comp1 = "reflecteren"
-            console.log(1)
-            console.log(comp1)
-        } else {
-            comp2 = "reflecteren"
-            console.log(2)
-            console.log(comp2)
-        }
-    }
-    if(document.getElementById("Plannen-kies").style.display == 'inline-block'){
-        if (comp1 == ""){
-            comp1 = "plannen"
-            console.log(1)
-            console.log(comp1)
-        } else if (comp2 == ""){
-            comp2 = "plannen"
-            console.log(2)
-            console.log(comp2)
-        } else {
-            comp3 = "plannen"
-            console.log(3)
-            console.log(comp3)
-        }
-    }
-    if(document.getElementById("Boodschap-kies").style.display == 'inline-block'){
-        if (comp1 == ""){
-            comp1 = "boodschap overbrengen"
-            console.log(1)
-            console.log(comp1)
-        } else if (comp2 == ""){
-            comp2 = "boodschap overbrengen"
-            console.log(2)
-            console.log(comp2)
-        } else if (comp3 == ""){
-            comp3 = "boodschap overbrengen"
-            console.log(3)
-            console.log(comp3)
-        } else {
-            comp4 = "boodschap overbrengen"
-            console.log(4)
-            console.log(comp4)
-        }
-    }
-    if(document.getElementById("Pro-actief-kies").style.display == 'inline-block'){
-        if (comp1 == ""){
-            comp1 = "pro-actief werken"
-            console.log(1)
-            console.log(comp1)
-        } else if (comp2 == ""){
-            comp2 = "pro-actief werken"
-            console.log(2)
-            console.log(comp2)
-        } else if (comp3 == ""){
-            comp3 = "pro-actief werken"
-            console.log(3)
-            console.log(comp3)
-        } else if (comp4 == ""){
-            comp4 = "pro-actief werken"
-            console.log(4)
-            console.log(comp4)
-        } else {
-            comp5 = "pro-actief werken"
-            console.log(5)
-            console.log(comp5)
-        }
-    }
-    if(document.getElementById("Aanpassingsvermogen-kies").style.display == 'inline-block'){
-        if (comp1 == ""){
-            comp1 = "aanpassingsvermogen"
-            console.log(1)
-            console.log(comp1)
-        } else if (comp2 == ""){
-            comp2 = "aanpassingsvermogen"
-            console.log(2)
-            console.log(comp2)
-        } else if (comp3 == ""){
-            comp3 = "aanpassimgsvermogen"
-            console.log(3)
-            console.log(comp3)
-        } else if (comp4 == ""){
-            comp4 = "aanpassingsvermogen"
-            console.log(4)
-            console.log(comp4)
-        } else if(comp5 == ""){
-            comp5 = "aanpassingsvermogen"
-            console.log(5)
-            console.log(comp5)
-        } else {
-            comp6 = "aanpassingsvermogen"
-            console.log(6)
-            console.log(comp6)
-        }
-    }
-
+    
     var titel = document.getElementById("popup-title-input").value;
     var tekst = document.getElementById("popup-pulse-context").value;
+    if (emoji == "" || titel == "" || tekst == ""){
+        if (emoji == ""){
+            document.getElementById("foutmelding-emoji").innerText = "Kies een emoji!"
+        }
+        if (titel == ""){
+            document.getElementById("foutmelding-titel").innerText = "Vul een titel in!"
+        } 
+        if (tekst == ""){
+            document.getElementById("foutmelding-tekst").innerText = "Vul een tekst in!"
+        }
+    } else {
+        console.log(emoji)
+        var comp1 = "";
+        var comp2 = "";
+        var comp3 = "";
+        var comp4 = "";
+        var comp5 = "";
+        var comp6 = "";
+        console.log(comp1)
+        if (document.getElementById("Samenwerken-kies").style.display == 'inline-block'){
+            comp1 = "samenwerken"
+            console.log(1)
+            console.log(comp1)
+        } 
+        if (document.getElementById("Reflecteren-kies").style.display == 'inline-block'){
+            if (comp1 == ""){
+                comp1 = "reflecteren"
+                console.log(1)
+                console.log(comp1)
+            } else {
+                comp2 = "reflecteren"
+                console.log(2)
+                console.log(comp2)
+            }
+        }
+        if(document.getElementById("Plannen-kies").style.display == 'inline-block'){
+            if (comp1 == ""){
+                comp1 = "plannen"
+                console.log(1)
+                console.log(comp1)
+            } else if (comp2 == ""){
+                comp2 = "plannen"
+                console.log(2)
+                console.log(comp2)
+            } else {
+                comp3 = "plannen"
+                console.log(3)
+                console.log(comp3)
+            }
+        }
+        if(document.getElementById("Boodschap-kies").style.display == 'inline-block'){
+            if (comp1 == ""){
+                comp1 = "boodschap overbrengen"
+                console.log(1)
+                console.log(comp1)
+            } else if (comp2 == ""){
+                comp2 = "boodschap overbrengen"
+                console.log(2)
+                console.log(comp2)
+            } else if (comp3 == ""){
+                comp3 = "boodschap overbrengen"
+                console.log(3)
+                console.log(comp3)
+            } else {
+                comp4 = "boodschap overbrengen"
+                console.log(4)
+                console.log(comp4)
+            }
+        }
+        if(document.getElementById("Pro-actief-kies").style.display == 'inline-block'){
+            if (comp1 == ""){
+                comp1 = "pro-actief werken"
+                console.log(1)
+                console.log(comp1)
+            } else if (comp2 == ""){
+                comp2 = "pro-actief werken"
+                console.log(2)
+                console.log(comp2)
+            } else if (comp3 == ""){
+                comp3 = "pro-actief werken"
+                console.log(3)
+                console.log(comp3)
+            } else if (comp4 == ""){
+                comp4 = "pro-actief werken"
+                console.log(4)
+                console.log(comp4)
+            } else {
+                comp5 = "pro-actief werken"
+                console.log(5)
+                console.log(comp5)
+            }
+        }
+        if(document.getElementById("Aanpassingsvermogen-kies").style.display == 'inline-block'){
+            if (comp1 == ""){
+                comp1 = "aanpassingsvermogen"
+                console.log(1)
+                console.log(comp1)
+            } else if (comp2 == ""){
+                comp2 = "aanpassingsvermogen"
+                console.log(2)
+                console.log(comp2)
+            } else if (comp3 == ""){
+                comp3 = "aanpassimgsvermogen"
+                console.log(3)
+                console.log(comp3)
+            } else if (comp4 == ""){
+                comp4 = "aanpassingsvermogen"
+                console.log(4)
+                console.log(comp4)
+            } else if(comp5 == ""){
+                comp5 = "aanpassingsvermogen"
+                console.log(5)
+                console.log(comp5)
+            } else {
+                comp6 = "aanpassingsvermogen"
+                console.log(6)
+                console.log(comp6)
+            }
+        }
 
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+        
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-    raw = JSON.stringify({"titel":titel,"tekst":tekst, "comp1":comp1, "comp2":comp2, "comp3":comp3, "comp4":comp4, "comp5":comp5, "comp6":comp6});
-    // raw = JSON.stringify({"titel":titel,"tekst":tekst,"comp1":comp1})
-    console.log(raw)
-    //Hier staan de fetch option
-    var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-    };
+        raw = JSON.stringify({"titel":titel,"tekst":tekst, "comp1":comp1, "comp2":comp2, "comp3":comp3, "comp4":comp4, "comp5":comp5, "comp6":comp6, "emoji":emoji });
+        // raw = JSON.stringify({"titel":titel,"tekst":tekst,"comp1":comp1};
+        console.log(raw)
+        //Hier staan de fetch option
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
 
-    //Hier word gefetch en gekeken of er error zijn en de response word omgezet naar text en vervolgens word dat geconsole logged
-    fetch("http://127.0.0.1:5050/create", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-    
+        //Hier word gefetch en gekeken of er error zijn en de response word omgezet naar text en vervolgens word dat geconsole logged
+        fetch("http://127.0.0.1:5050/create", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
 }
