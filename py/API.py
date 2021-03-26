426dc0e7f458fbd495c2bc33f99964df3d5d48b3
Pjotr Kuiper <pjotr.kuiper@student.hu.nl>
2021-03-26 10:40:20 +0100

    api Pjotr update

--- py/API.py
+++ py/API.py
@@ -14,6 +14,7 @@ driver = GraphDatabase.driver(uri=uri,auth=(username,pwd))
 session = driver.session()
 api = Flask(__name__)
 
+# aanmaken van een Pulse API
 @api.route("/create", methods=["GET", "POST"])
 def create_node():
     req_data = request.get_json()
@@ -28,7 +29,7 @@ def create_node():
     comp6 = req_data['comp6']
     if (comp1 == ""):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -40,7 +41,7 @@ def create_node():
         print(1)
     if ((comp1 != "") and (comp2 == "")):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -51,7 +52,7 @@ def create_node():
             return (str(e))
     if ((comp2 != "") and (comp3 == "")):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -63,7 +64,7 @@ def create_node():
         print(3)
     if ((comp3 != "") and (comp4 == "")):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -75,7 +76,7 @@ def create_node():
         print(4)
     if ((comp4 != "") and (comp5 == "")):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -86,7 +87,7 @@ def create_node():
             return (str(e))
     if ((comp5 != "") and (comp6 == "")):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4,comp5:$comp5})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4,comp5:$comp5})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -98,7 +99,7 @@ def create_node():
         print(6)
     if (comp6 != ""):
         q1 = """
-        create (n:checkpoint {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4,comp5:$comp5,comp6:$comp6})
+        CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4,comp5:$comp5,comp6:$comp6})
         """
         map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
                "comp5": comp5, "comp6": comp6}
@@ -112,12 +113,153 @@ def create_node():
 @api.route("/api/display",methods=["GET","POST"])
 def display_node():
     q1="""
-    match (n:persoon) return n
+    MATCH (p:Pulse) RETURN p
     """
     results=session.run(q1)
     data=results.data()
     print(data)
     return(jsonify(data))
 
+@api.route("/create",methods=["GET","POST"])
+def create_node3():
+    req_data = request.get_json()
+    emoji = req_data['emoji']
+    q1="""
+    CREATE (p:Pulse {emoji:$emoji})
+    """
+    map={"emoji":emoji}
+    try:
+        session.run(q1,map)
+        return 'succesfull'
+    except Exception as e:
+        return (str(e))
+
+# API voor het bewerken van een Checkpoint -->
+@api.route("/ophalen",methods=["GET","POST"])
+def ophalen_node():
+    q2="""
+    MATCH (p:Pulse {id:$id}) 
+    RETURN p:Pulse
+    """
+    results=session.run(q2)
+    data=results.data()
+    print(data)
+    return(jsonify(data))
+
+@api.route("/bewerken",methods=["GET","POST"])
+def bewerken_node():
+    req_data = request.get_json()
+    titel = req_data['titel']
+    tekst = req_data['tekst']
+    emoji = req_data['emoji']
+    comp1 = req_data['comp1']
+    comp2 = req_data['comp2']
+    comp3 = req_data['comp3']
+    comp4 = req_data['comp4']
+    comp5 = req_data['comp5']
+    comp6 = req_data['comp6']
+    if (comp1 == ""):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            SET (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+            print(1)
+
+    if ((comp1 != "") and (comp2 == "")):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            CREATE (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+    if ((comp2 != "") and (comp3 == "")):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            SET (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+            print(3)
+    if ((comp3 != "") and (comp4 == "")):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            SET (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+            print(4)
+    if ((comp4 != "") and (comp5 == "")):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            SET (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+            print(5)
+    if ((comp5 != "") and (comp6 == "")):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            SET (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4,comp5:$comp5})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+            print(6)
+    if (comp6 != ""):
+            q1 = """
+            MATCH(p:Pulse{id:$id})
+            SET (p:Pulse {titel:$titel,tekst:$tekst,emoji:$emoji,comp1:$comp1,comp2:$comp2,comp3:$comp3,comp4:$comp4,comp5:$comp5,comp6:$comp6})
+            """
+            map = {"titel": titel, "tekst": tekst, "emoji": emoji, "comp1": comp1, "comp2": comp2, "comp3": comp3, "comp4": comp4,
+                "comp5": comp5, "comp6": comp6}
+            try:
+                session.run(q1, map)
+                return 'succesfull'
+            except Exception as e:
+                return (str(e))
+            print(7)
+
+    # q2="""
+    # MATCH (p.Pulse)
+    # SET (p.Titel)
+    # RETURN p:Pulse
+    # """
+    # results=sessopm.rum(q2)
+    # data=results.date()
+    # print(data)
+    # return(jsonify(data))
+
 if __name__=="__main__":
     api.run(port=5050)
+
