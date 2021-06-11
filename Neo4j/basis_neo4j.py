35e4a955065d77f27c30523c672763f77bbce6bf
ghostlykraken <70856032+ghostlykraken@users.noreply.github.com>
2021-06-11 09:58:22 +0200

    update basis_neo4j
    onnodige dingen weggehaald

--- Neo4j/basis_neo4j.py
+++ Neo4j/basis_neo4j.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,jsonify,redirect,render_template,request,jsonify
 from neo4j import GraphDatabase
-import csv,secrets,requests
+import csv,secrets
 
 #establish the connection
 with open(r'C:\Users\caspe\Documents\GitHub\PeLeP\Neo4j\Test-API.text') as f1:
@@ -14,7 +14,7 @@ session = driver.session()
 api = Flask(__name__)
 
 # werkende data ophalen
-# @api.route("/Test",methods=["GET"])
+# @api.route("/test_ophalen",methods=["GET"])
 # def display_node():
 #     q1="""
 #     MATCH (p:pulse) RETURN p
@@ -23,7 +23,6 @@ api = Flask(__name__)
 #     data=results.data()
 #     print(data)
 #     return(jsonify(data))
-
 
 # werkende token post
 # run this query in Neo4j: CREATE CONSTRAINT ON (p:plulse) ASSERT p.token IS UNIQUE
@@ -43,28 +42,6 @@ def test_token():
         token = secrets.token_urlsafe()
         session.run(q1, map, token = token)
         return 'another try'
-
-# token post loop in progress 
-# @api.route("/test_token", methods=["POST"])
-# def test_token():
-#     naam = "Test 8"
-#     token = "565"
-#     resp = request.post
-#     print(token)
-#     q1="""
-#     CREATE (p:pulse {naam:$naam, token:$token})
-#     """
-#     map = {"naam":naam, "token":token}
-#     while True:   
-#         try:
-#         session.run(q1, map, token = token)
-#         return "first try"
-#     else:
-#         resp.statuscode != 200:
-#         token =secrets.token_urlsafe()
-#         session.run(q1, map, token = token)
-#         return "not first try"
-
 
 if __name__=="__main__":
     api.run(debug=True)
